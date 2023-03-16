7b2df612c923ef251ea56af49bf5057f9394869f
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-15 23:29:05 -0700

    updated HTML and script.js

--- script.js
+++ script.js
@@ -1,111 +1,101 @@
-let buttons = document.querySelectorAll("#answers")
-let time = document.getElementById("time")
-let welcomePage = document.getElementById("section-1")
-let highScore = document.getElementById("results-page")
-let questionPage = document.getElementById("question-page")
-let count = 100
-let finalScore = ''
+let buttons = document.querySelectorAll('.answer-item');
+let time = document.getElementsByClassName('.time');
+let welcomePage = document.getElementById('section-1');
+let highScore = document.getElementById('results-page');
+let questionPage = document.getElementById('question-page');
+let count = 100;
+let finalScore = '';
 let timer;
-let questionsIndex = 0
+let questionsIndex = 0;
 let questions = [
-    {
-        questions: "Commonly used data types DO Not Include:",
-        answers: ["string", "Booleans","Alerts", "Numbers"],
-        correctAnswer:"Alerts"
-    },
-    {
-        questions: " The condition in an if/else statement is enclosed with _____ ",
-        answers: ["quotes", " curly brackets", "parenthesis","square brackets"],
-        correctAnswer:"quotes"
-    },
-    {
-        questions: "Arrays in JavaScript can be used to store_____ ",
-        answers: ["numbers and strings","other arrays", "booleans","all of the above"],
-        correctAnswer:"all of the above"
-    },
-    {
-        questions: " String values must be enclosed within __ when being assigned to variables",
-        answers: ["commas","curly brackets","quotes", "parenthesis"],
-        correctAnswer:"comma"
-    },
+  {
+    questions: 'Commonly used data types DO Not Include:',
+    answers: ['string', 'Booleans', 'Alerts', 'Numbers'],
+    correctAnswer: 'Alerts',
+  },
+  {
+    questions: ' The condition in an if/else statement is enclosed with _____ ',
+    answers: ['quotes', ' curly brackets', 'parenthesis', 'square brackets'],
+    correctAnswer: 'quotes',
+  },
+  {
+    questions: 'Arrays in JavaScript can be used to store_____ ',
+    answers: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    correctAnswer: 'all of the above',
+  },
+  {
+    questions:
+      ' String values must be enclosed within __ when being assigned to variables',
+    answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+    correctAnswer: 'quotes',
+  },
 
-    {
-        questions: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["JavaScript", "terminal/bash","for loops","console.log"],
-        correctAnswer:"console.log"
-    },
-
+  {
+    questions:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+    correctAnswer: 'console.log',
+  },
 ];
 
 function timerCountdown() {
-
-    timer = setInterval(function () {
-        count--;
-        time.textContent = count
-        // console.log(buttons)
-        if (count === 0) {
-    finish();
-
-        }
-    }, 1000);
+  timer = setInterval(function () {
+    count--;
+    time.textContent = count;
+    if (count === 0) {
+      finish();
+    }
+  }, 1000);
 }
 
 function startQuiz() {
-    welcomePage.setAttribute("style", "display:none")
-    timerCountdown()
-    loadQuestions()
+  welcomePage.setAttribute('style', 'display:none');
+  timerCountdown();
+  loadQuestions();
 }
 
 function loadQuestions() {
-    if (questionsIndex === questions.length) {
-    finish()
-        // quiz is done.
-    }
-    const question = questions[questionsIndex]
+  if (questionsIndex >= questions.length) {
+    finish();
+    // quiz is done.
+  }
+  const question = questions[questionsIndex];
 
-    document.getElementById("answers").innerHTML = ""
+  document.getElementById('answers').innerHTML = '';
 
-    document.getElementById("question").innerHTML= question.questions
+  document.getElementById('question').innerHTML = question.questions;
 
+  question.answers.forEach((answer) => {
+    var li = document.createElement('li');
+    li.innerHTML = answer;
+    li.classList.add('answer-item');
+    document.getElementById('answers').appendChild(li);
 
-    question.answers.forEach(answer => {
-        var li = document.createElement("li")
-        li.innerHTML = answer
-        li.classList.add("answer-item")
-        document.getElementById("answers").appendChild(li)
-        console.log(li)
-        // li.addEventListener('click', function (event) {
-        //     // get the clicked element
-        //     const clickedElement = event.target
-        //     const lianswer = event.target.innerHTML
-        //     answerQuestion(event.target.innerHTML)
-        // });
-    
-
-    });
+    li.addEventListener('click', checkAnswer);
+  });
 }
 
-function answerQuestion(answer) {
-    const question = questions[questionsIndex]
-    if (answer!== question.correctAnswer) {
-        count -= 20.
-
- 
-    }
-    questionsIndex ++
-    loadQuestions()
+function checkAnswer(event) {
+  const question = questions[questionsIndex];
+  if (event.target.innerHTML !== question.correctAnswer) {
+    count -= 20;
+  }
+  questionsIndex++;
+  loadQuestions();
 }
 
 function finish() {
-    clearInterval(timer)
-    ScoreSection()
-
+  clearInterval(timer);
+  ScoreSection();
 }
 
 function ScoreSection() {
-// if you finish before the time ends let final score equal time left. 
-// questionPage.setAttribute("style", "display:none")
-finalScore = count
-
-    console.log("HELLO!")
+  // if you finish before the time ends let final score equal time left.
+  // questionPage.setAttribute("style", "display:none")
+console.log(count);
 }
