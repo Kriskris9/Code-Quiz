2469833783fab7641a0c6ed5381fbb1e476db9a8
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-15 22:09:20 -0700

    fixed timer and added variables to script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let buttons = document.querySelectorAll("#answers")
 let time = document.getElementById("time")
 let welcomePage = document.getElementById("section-1")
+let highScore = document.getElementById("results-page")
 let questionPage = document.getElementById("question-page")
 let count = 100
 let finalScore = ''
@@ -40,58 +42,75 @@ function timerCountdown() {
     timer = setInterval(function () {
         count--;
         time.textContent = count
+        // console.log(buttons)
         if (count === 0) {
        
-            finish();
+    finish();
 
         }
     }, 1000);
 }
 
 function startQuiz() {
-    welcomePage.setAttribute("style", "display:none");
+    welcomePage.setAttribute("style", "display:none")
     timerCountdown()
     loadQuestions()
 }
 
 function loadQuestions() {
-    if (questionsIndex >= questions.length) {
-return;
+    if (questionsIndex === questions.length) {
+    finish()
         // quiz is done.
     }
-    const question = questions[questionsIndex];
+    const question = questions[questionsIndex]
 
-    document.getElementById("answers").innerHTML = "";
+    document.getElementById("answers").innerHTML = ""
 
     document.getElementById("question").innerHTML= question.questions
 
 
     question.answers.forEach(answer => {
-        var li = document.createElement("li");
-        li.innerHTML = answer;
-        li.classList.add("answer-item");
-        document.getElementById("answers").appendChild(li);
-        li.addEventListener('click', function (event) {
-            // get the clicked element
-            const clickedElement = event.target;
-            const lianswer = event.target.innerHTML
-            answerQuestion(event.target.innerHTML);
+        var li = document.createElement("li")
+        li.innerHTML = answer
+        li.classList.add("answer-item")
+        document.getElementById("answers").appendChild(li)
+        console.log(li)
+        // li.addEventListener('click', function (event) {
+        //     // get the clicked element
+        //     const clickedElement = event.target
+        //     const lianswer = event.target.innerHTML
+        //     answerQuestion(event.target.innerHTML)
           
-        });
+        // });
+    
 
     });
 }
+
+
+
+
 function answerQuestion(answer) {
-    const question = questions[questionsIndex];
+    const question = questions[questionsIndex]
     if (answer!== question.correctAnswer) {
         count -= 20.
 
  
     }
-    questionsIndex ++;
+    questionsIndex ++
     loadQuestions()
 }
 
 function finish() {
-    clearInterval(timer);
+    clearInterval(timer)
+    ScoreSection()
+
+}
+
+function ScoreSection() {
+// if you finish before the time ends let final score equal time left. 
+// questionPage.setAttribute("style", "display:none")
+finalScore = count
+
+    console.log("HELLO!")
 }
